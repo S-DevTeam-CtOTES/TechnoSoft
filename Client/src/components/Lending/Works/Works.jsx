725b985af422d90c5089807caad037f671f5ccc0
ITDOBRO <avdushinbeaver1@gmail.com>
2023-08-06 01:20:58 +0300

    Added: SPA Optimization Site Info Fixed container responsive paddings

--- Client/src/components/Lending/Works/Works.jsx
+++ Client/src/components/Lending/Works/Works.jsx
@@ -1,48 +1,43 @@
-import React, {useState} from 'react'
-import Tabs from './Tabs/Tabs'
+import React, { useState } from "react";
+import Tabs from "./Tabs/Tabs";
 
-import AccardionItem from './AccardionItem/AccardionItem'
-import './Works.scss'
-import { data } from './data/data'
+import AccardionItem from "./AccardionItem/AccardionItem";
+import "./Works.scss";
+import { data } from "./data/data";
 
 const Works = () => {
-
   const [selected, setSelected] = useState(null);
 
   const toggle = (i) => {
-    if (selected === i ) {
-      return setSelected(null)
+    if (selected === i) {
+      return setSelected(null);
     }
-    setSelected(i)
-  }
+    setSelected(i);
+  };
 
   return (
-    <div className='Works' id='Works'>
-        <div className="container">
-            <div className="Works__wrapper">
-                <div className="Works__wrapper-title title">
-                    ВЫПОЛНЕННЫЕ РАБОТЫ
-                </div>
+    <div className="Works" id="Works">
+      <div className="container">
+        <div className="Works__wrapper">
+          <div className="Works__wrapper-title title">ВЫПОЛНЕННЫЕ РАБОТЫ</div>
 
-                <div className="Works__wrapper-accardion">
-                  
-                  {data.map((item, i) => {
-                    return (
-                      <AccardionItem 
-                        key={i}
-                        data={item} 
-                        index={i} 
-                        toggle={toggle}
-                        selected={selected}
-                        />
-                    )
-                  })}
-
-                </div>
-            </div>    
+          <div className="Works__wrapper-accardion">
+            {data.map((item, i) => {
+              return (
+                <AccardionItem
+                  key={i}
+                  data={item}
+                  index={i}
+                  toggle={toggle}
+                  selected={selected}
+                />
+              );
+            })}
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
